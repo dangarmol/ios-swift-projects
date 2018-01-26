dd6e23c4f0c4501cd253091e5b9ab9db24031122
Daniel Garcia Molero <dangar13@icloud.com>
2018-01-26 13:31:37 +0000

    Added HelloWorld test project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Daniel García Molero on 26/01/2018.
+//  Copyright © 2018 Daniel García Molero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
