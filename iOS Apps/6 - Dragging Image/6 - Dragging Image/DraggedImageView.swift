397bb5ed3505fad644aceea402e8b0694dca94cf
Daniel Garcia Molero <dangar13@icloud.com>
2018-02-26 13:49:44 +0000

    Finished Task 6, Dragging Image

--- iOS Apps/6 - Dragging Image/6 - Dragging Image/DraggedImageView.swift
+++ iOS Apps/6 - Dragging Image/6 - Dragging Image/DraggedImageView.swift
@@ -10,18 +10,20 @@ import UIKit
 
 class DraggedImageView: UIImageView {
 
-    var startLocation: CGPoint? //Global variable and type
+    var startLocation: CGPoint? //Global variable and type (point in the screen).
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //When touch begins, records the initial position.
         startLocation = touches.first?.location(in:self)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //When touch moves, find the coordinates of the current touch and set the center position of the image.
         let currentLocation = touches.first?.location(in:self)
         let dx = currentLocation!.x - startLocation!.x
         let dy = currentLocation!.y - startLocation!.y
         
-        //self.center = CGPointMake(self.center.x + dx, self.center.y + dy) //Old Swift Version
+        //self.center = CGPointMake(self.center.x + dx, self.center.y + dy) //Old Swift Version, new version below.
         self.center = CGPoint(x: self.center.x + dx, y: self.center.y + dy)
     }
 }
